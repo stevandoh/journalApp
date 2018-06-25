df9995985092eb4b9f132ff9e25149425e604ae1
Steve Dawsonn-Andoh <stevandoh@gmail.com>
2018-06-25 16:22:21 +0000

    EntryEntity model created

---
+++ app/src/main/java/com/stevandoh/journalapp/journalapp/models/EntryEntity.java
@@ -0,0 +1,56 @@
+package com.stevandoh.journalapp.journalapp.models;
+
+import java.util.Date;
+
+public class EntryEntity {
+    private int id;
+    private Date date;
+    private String content;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public EntryEntity() {
+    }
+
+    public EntryEntity(int id, Date date, String content) {
+        this.id = id;
+        this.date = date;
+        this.content = content;
+    }
+
+    public EntryEntity(Date date, String content) {
+        this.date = date;
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return "EntryEntity{" +
+                "id=" + id +
+                ", date=" + date +
+                ", content='" + content + '\'' +
+                '}';
+    }
+}
